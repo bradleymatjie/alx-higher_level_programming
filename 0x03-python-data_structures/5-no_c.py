b48701fe833b466c55afe9c46f448dd711a55760
bradleymatjie <bradleysaint45@gmail.com>
2022-06-10 05:55:40 -0700

    0x03

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,7 @@
 def no_c(my_string):
     new_string = ''
     for a in my_string:
-        if a != 'c' or a != 'C':
-            new_string += a
-    return new_string
-
+        if a == 'c' or a == 'C':
+            a = ''
+    return my_string
+no_c("i can see u")
